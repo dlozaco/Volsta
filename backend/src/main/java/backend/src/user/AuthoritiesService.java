acff837437213d80d5140272a51358ea66b95e63
David Lozano <dlozaco975@gmail.com>
2026-05-26 23:32:42 +0200

    feat(back): crud service
    Refs #2

---
+++ backend/src/main/java/backend/src/user/AuthoritiesService.java
@@ -0,0 +1,32 @@
+package backend.src.user;
+
+import backend.src.exceptions.ResourceNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class AuthoritiesService {
+
+
+    private final AuthoritiesRepository authoritiesRepository;
+
+    public AuthoritiesService(AuthoritiesRepository authoritiesRepository){
+        this.authoritiesRepository = authoritiesRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Authorities> findAllAuthorities(){
+        return authoritiesRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Authorities findByAuthoritiy(String authority){
+        return authoritiesRepository.findByAuthority(authority)
+                .orElseThrow(() -> new ResourceNotFoundException("Authority", "Name", authority));
+    }
+
+}
