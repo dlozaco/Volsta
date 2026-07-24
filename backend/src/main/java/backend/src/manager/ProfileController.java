fe3071cbe507a1deafda60dbae920c8e321de8e7
David Lozano <dlozaco975@gmail.com>
2026-07-24 21:21:37 +0200

    feat(back): update profile endpoint

--- backend/src/main/java/backend/src/manager/ProfileController.java
+++ backend/src/main/java/backend/src/manager/ProfileController.java
@@ -6,9 +6,9 @@ import backend.src.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/profile")
@@ -28,6 +28,23 @@ public class ProfileController {
         String username = authentication.getName();
         User user = userService.findUserByName(username);
         Manager manager = managerService.findByUserId(user.getId());
+        ManagerProfile managerProfile = ProfileHelpers.managerToProfile(manager);
+
+        return ResponseEntity.ok(managerProfile);
+    }
+
+    @PutMapping("/manager")
+    public ResponseEntity<?> updateManagerProfile(Authentication authentication, @RequestBody Map<String, String> body){
+        String username = authentication.getName();
+        User user = userService.findUserByName(username);
+        Manager manager = managerService.findByUserId(user.getId());
+
+        if (body.containsKey("name")) manager.setName(body.get("name"));
+        if (body.containsKey("surname")) manager.setSurname(body.get("surname"));
+        if (body.containsKey("email")) manager.setEmail(body.get("email"));
+        if (body.containsKey("phoneNumber")) manager.setPhoneNumber(body.get("phoneNumber"));
+
+        managerService.create(manager);
         ManagerProfile managerProfile = ProfileHelpers.managerToProfile(manager);
 
         return ResponseEntity.ok(managerProfile);
