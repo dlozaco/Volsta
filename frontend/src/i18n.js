e445837b769558941f84a15e45a98171d84bc271
David Lozano <dlozaco975@gmail.com>
2026-08-11 13:41:50 +0200

    feat(front): profile english and spanish

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -11,7 +11,7 @@ i18n
         fallbackLng: 'es',
         supportedLngs: ['es', 'en'],
         debug: false,
-        ns: ['home', 'dashboard', 'sidebar', 'teams', 'teamCreate'],
+        ns: ['home', 'dashboard', 'sidebar', 'teams', 'teamCreate', 'profile'],
         interpolation: {
             escapeValue: false,
         },
