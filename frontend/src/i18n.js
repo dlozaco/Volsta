f9c24fe9dbd5c264ad791c38d4d60e8a63be25b4
David Lozano <dlozaco975@gmail.com>
2026-08-13 12:17:59 +0200

    chore(front): i18n config

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -11,7 +11,7 @@ i18n
         fallbackLng: 'es',
         supportedLngs: ['es', 'en'],
         debug: false,
-        ns: ['home', 'dashboard', 'sidebar', 'teams', 'teamCreate', 'profile', 'matches'],
+        ns: ['home', 'dashboard', 'sidebar', 'teams', 'teamCreate', 'profile', 'matches', 'teamStats'],
         interpolation: {
             escapeValue: false,
         },
